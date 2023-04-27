8573134f695b8ca559243a4adc702ede770c30aa
Jon Skeet <jonskeet@google.com>
2023-04-27 10:31:44 +0000

    Show the built generator

--- bazeltest.sh
+++ bazeltest.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -o
+
 bazel build @gapic_generator_csharp//rules_csharp_gapic:csharp_gapic_generator_binary
+
+ls bazel-bin/external/gapic_generator_csharp/rules_csharp_gapic/out/Google.Api.Generator/bin/Debug/net6.0/
 
 #bazel build //google/cloud/functions/v2:functions_proto_with_info
 #bazel build //google/cloud/functions/v2:functions_csharp_grpc
